e38e0ea4c6f3da1fa368e551ed3948070540888d
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-08-21 13:31:32 -0400

    turned service key int env variable and imported into firebaseAdmin file

--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import { initializeApp, getApps, App, getApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 
-const serviceKey = require('../service_key.json');
+const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_KEY as string);
 
 let app: App;
 
